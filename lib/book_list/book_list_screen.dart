6f37e82a125f396a88319511a255d67f9422ffe6
minhaneul <onlynforever@gmail.com>
2022-08-09 23:20:42 +0900

    Fix: BookListScreen backgroundColor 수정

--- lib/book_list/book_list_screen.dart
+++ lib/book_list/book_list_screen.dart
@@ -20,56 +20,59 @@ class BookListScreen extends StatelessWidget {
           child: Text('내 도서 리스트', style: TextStyle(color: Colors.black)),
         ),
       ),
-      body: StreamBuilder<QuerySnapshot<Book>>(
-          stream: viewModel.booksStream,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return const Text('Something went wrong');
-            }
+      body: Container(
+        color: const Color(0xFFFDFAF3),
+        child: StreamBuilder<QuerySnapshot<Book>>(
+            stream: viewModel.booksStream,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return const Text('Something went wrong');
+              }
 
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Text("Loading");
-            }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Text("Loading");
+              }
 
-            return ListView(
-              children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                Book book = document.data()! as Book;
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: ListTile(
-                    leading: Image.network(
-                      book.imageUrl,
-                      width: 70,
-                    ),
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(book.title),
-                        const SizedBox(height: 3),
-                        Text(book.author, style: const TextStyle(fontSize: 14, color: Colors.black54),),
-                      ],
-                    ),
-                    visualDensity: const VisualDensity(vertical: 4),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => UpdateBookScreen(document)),
-                      );
-                    },
-                    trailing: IconButton(
-                      splashRadius: 20,
-                      icon: const Icon(Icons.delete),
-                      highlightColor: const Color(0x66A351F7),
-                      onPressed: () {
-                        BookListViewModel().deleteBook(document: document);
+              return ListView(
+                children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                  Book book = document.data()! as Book;
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: ListTile(
+                      leading: Image.network(
+                        book.imageUrl,
+                        width: 70,
+                      ),
+                      title: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(book.title),
+                          const SizedBox(height: 3),
+                          Text(book.author, style: const TextStyle(fontSize: 14, color: Colors.black54),),
+                        ],
+                      ),
+                      visualDensity: const VisualDensity(vertical: 4),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => UpdateBookScreen(document)),
+                        );
                       },
+                      trailing: IconButton(
+                        splashRadius: 20,
+                        icon: const Icon(Icons.delete),
+                        highlightColor: const Color(0x66A351F7),
+                        onPressed: () {
+                          BookListViewModel().deleteBook(document: document);
+                        },
+                      ),
                     ),
-                  ),
-                );
-              }).toList(),
-            );
-          }),
+                  );
+                }).toList(),
+              );
+            }),
+      ),
       floatingActionButton: SpeedDial(
         icon: Icons.person,
         backgroundColor: const Color(0xFFFFEC60),
