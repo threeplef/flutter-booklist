6bc8a6238fe3dffaedc984d1303d268ad609a27a
minhaneul <onlynforever@gmail.com>
2022-08-11 11:17:00 +0900

    Fix: updateBook UI 수정 및 오류 수정

--- lib/update_book/update_book_screen.dart
+++ lib/update_book/update_book_screen.dart
@@ -1,7 +1,9 @@
-import 'package:book_list_app/model/book.dart';
+import 'dart:typed_data';
+
 import 'package:book_list_app/update_book/update_book_view_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UpdateBookScreen extends StatefulWidget {
   final DocumentSnapshot document;
@@ -18,6 +20,10 @@ class _UpdateBookScreenState extends State<UpdateBookScreen> {
 
   final viewModel = UpdateBookViewModel();
 
+  final ImagePicker _picker = ImagePicker();
+
+  Uint8List? _bytes;
+
   @override
   void dispose() {
     _titleTextController.dispose();
@@ -29,47 +35,115 @@ class _UpdateBookScreenState extends State<UpdateBookScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('수정'),
+        iconTheme: const IconThemeData(color: Colors.black),
+        backgroundColor: const Color(0xFFFFEC60),
+        title: const Text('수정', style: TextStyle(color: Colors.black)),
+        centerTitle: true,
       ),
-      body: Column(
-        children: [
-          TextField(
-            controller: _titleTextController,
-            decoration: InputDecoration(
-              border: const OutlineInputBorder(),
-              hintText: widget.document['title'],
+      body: Container(
+        color: const Color(0xFFFDFAF3),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: GestureDetector(
+                    onTap: () async {
+                      XFile? image =
+                          await _picker.pickImage(source: ImageSource.gallery);
+                      if (image != null) {
+                        // byte array
+                        _bytes = await image.readAsBytes();
+
+                        setState(() {});
+                      }
+                    },
+                    child: SizedBox(
+                      width: 200,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: _bytes == null
+                              ? Image.network(widget.document['imageUrl'],
+                                  fit: BoxFit.cover)
+                              : Image.memory(_bytes!, fit: BoxFit.cover)),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
+                  child: TextField(
+                    controller: _titleTextController,
+                    decoration: InputDecoration(
+                      enabledBorder: const OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black26, width: 1.0)),
+                      focusedBorder: const OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xFFA351F7), width: 1.0)),
+                      hintText: widget.document['title'],
+                      contentPadding: const EdgeInsets.fromLTRB(10, 15, 0, 15),
+                      isCollapsed: true,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
+                  child: TextField(
+                    controller: _authorTextController,
+                    decoration: InputDecoration(
+                      enabledBorder: const OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black26, width: 1.0)),
+                      focusedBorder: const OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xFFA351F7), width: 1.0)),
+                      hintText: widget.document['author'],
+                      contentPadding: const EdgeInsets.fromLTRB(10, 15, 0, 15),
+                      isCollapsed: true,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 340,
+                  height: 45,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      viewModel
+                          .updateBook(
+                            document: widget.document,
+                            title: _titleTextController.text,
+                            author: _authorTextController.text,
+                            bytes: _bytes,
+                          )
+                          .then((_) => Navigator.pop(context))
+                          .catchError((e) {
+                        // 에러가 났을 때
+                        final snackBar = SnackBar(
+                          content: Text(e.toString()),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                        primary: const Color(0xFFFFEC60),
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50))),
+                    child: const Text(
+                      '도서 수정',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFFA351F7)),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-          TextField(
-            controller: _authorTextController,
-            decoration: InputDecoration(
-              border: const OutlineInputBorder(),
-              hintText: widget.document['author'],
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          try {
-            // 에러가 날 것 같은 코드
-            viewModel.updateBook(
-              document: widget.document,
-              title: _titleTextController.text,
-              author: _authorTextController.text,
-            );
-            Navigator.pop(context);
-          } catch (e) {
-            // 에러가 났을 때
-            final snackBar = SnackBar(
-              content: Text(e.toString()),
-            );
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-          } finally {
-            // (옵션) 에러가 나거나, 안 나거나 무조건 마지막에 수행되는 블럭
-          }
-        },
-        child: const Icon(Icons.done),
+        ),
       ),
     );
   }
--- lib/update_book/update_book_view_model.dart
+++ lib/update_book/update_book_view_model.dart
@@ -1,30 +1,42 @@
-import 'package:book_list_app/model/book.dart';
+import 'dart:typed_data';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class UpdateBookViewModel {
-  final _db = FirebaseFirestore.instance
-      .collection('books')
-      .withConverter<Book>(
-          fromFirestore: (snapshot, _) => Book.fromJson(snapshot.data()!),
-          toFirestore: (book, _) => book.toJson());
+  final _db = FirebaseFirestore.instance;
+  final _storage = FirebaseStorage.instance;
 
-  void updateBook(
+  Future uploadImage(String fileName, Uint8List bytes) async {
+    final storageRef = _storage.ref().child('book_cover/$fileName.jpg');
+    await storageRef.putData(bytes);
+    String downloadUrl = await storageRef.getDownloadURL();
+    return downloadUrl;
+  }
+
+  Future updateBook(
       {required DocumentSnapshot document,
       required String title,
-      required String author}) {
-    bool isValid = title.isNotEmpty && author.isNotEmpty;
-
-    if (isValid) {
-      _db.doc(document.id).update({
+      required String author,
+      required Uint8List? bytes}) async {
+    if (title.isNotEmpty && author.isNotEmpty && bytes == null) {
+      await _db.collection('books').doc(document.id).update({
         "title": title,
         "author": author,
       });
-    } else if (title.isEmpty && author.isEmpty) {
-      throw '제목과 저자를 입력해주세요';
-    } else if (title.isEmpty) {
-      throw '제목을 입력해주세요';
+    } else if (title.isNotEmpty && author.isNotEmpty && bytes != null) {
+      String downloadUrl = await uploadImage(document.id, bytes);
+      await _db.collection('books').doc(document.id).update({
+        "title": title,
+        "author": author,
+        "imageUrl": downloadUrl,
+      });
+    } else if (author.isEmpty && title.isEmpty) {
+      return Future.error('제목과 저자를 입력해 주세요');
     } else if (author.isEmpty) {
-      throw '저자를 입력해주세요';
+      return Future.error('저자를 입력해 주세요');
+    } else if (title.isEmpty) {
+      return Future.error('제목을 입력해 주세요');
     }
   }
 }
